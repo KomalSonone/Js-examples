1fe1658b891476e20afc9fc64c0e1e15e04725d4
KomalSonone <118454481+KomalSonone@users.noreply.github.com>
2023-11-03 09:17:45 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+//variables are containers for storing data
+/* All JavaScript variables must be identified with unique names called as 
+identifiers(age, sum, totalVolume).*/
+// let and const have block scope.
+// let and const can not be redeclared(let can be reassigned).
+// let and const must be declared before use.
+//var is not preffered to use
+
+
+const companyName="Infosy";
+let employeeName = "John";
+var employeeId = 1309234;
+employeeCity="Nagpur";
+
+//companyName="TCS"; //not allowed
+
+console.table([companyName, employeeId, employeeName, employeeCity])
+
+// Not to use var as it does not have scope
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// JavaScript has dynamic types.
+
+
+let x="hello"
+console.log(typeof "John"); //string
+console.log(typeof 3.14); //number
+console.log(typeof NaN); //number
+console.log(typeof false); //boolean
+console.log(typeof ["asd","saf","fdg"]); //object
+console.log(typeof {name:'John', age:34}); //object
+console.log(typeof new Date()); //object
+console.log(typeof function () {}); //function
+console.log(typeof myCar); //undefined
+console.log(typeof null); //object
+console.log(typeof x);
